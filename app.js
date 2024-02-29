83156a410367972f9bd9918217d2f2ddcb69b1e5
estebanQuirogaH <juan.quiroga09@uptc.edu.co>
2024-02-29 11:59:24 -0500

    Cambio en direccion ip

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const cors = require('cors');
 const app = express();
 const fs = require('fs');
 
-const IP_ADDRESS = 'localhost'
+const IP_ADDRESS = '10.4.61.86'
 
 app.use(express.json()); 
 app.use(cors());
 
 let cars = [];
 
-// Endpoint para registrar el ingreso de un carro
 app.post('/cars', (req, res) => {
     const { name, license_plate, color } = req.body;
     const timestamp = new Date().toLocaleString();
@@ -19,17 +18,14 @@ app.post('/cars', (req, res) => {
     res.send('Car registered successfully');
 });
 
-
 app.post('/cars', (req, res) => {
     res.status(405).send('Method Not Allowed');
   });
 
-// Endpoint para listar los vehículos registrados
 app.get('/cars', (req, res) => {
   res.json(cars);
 });
 
-// Endpoint para retirar un carro
 app.patch('/cars', (req, res) => {
   const { license_plate } = req.body;
 
@@ -37,13 +33,11 @@ app.patch('/cars', (req, res) => {
   res.send('Car removed successfully');
 });
 
-
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} - ${new Date().toLocaleString()}`);
   next();
 });
 
-// Manejo de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
@@ -54,7 +48,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(3000, IP_ADDRESS, () => {
   console.log(`Servidor escuchando en http://${IP_ADDRESS}:3000`);
 });
-
 
 const logStream = fs.createWriteStream('server.log', { flags: 'a' });
 app.use((req, res, next) => {
